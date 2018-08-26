2746d520aa5b166854882b7d93551b5fa84c7b57
Skillz CMS <office@myjackson.at>
2018-08-26 14:10:46 +0200

    Add files via upload

---
+++ Framework/Initialize.php
@@ -0,0 +1,16 @@
+<?PHP 
+
+namespace Skillzcms\Framework;
+
+class Initialize {
+    
+    public function __construct() {
+       
+    }
+    
+    public static function startInit() {
+        echo "Skillz CMS is now initialized! Yay.";     
+        return; 
+    }
+    
+}
